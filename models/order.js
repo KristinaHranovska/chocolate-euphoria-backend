458ee7575faf6d77382b97c7727abac64b2a286c
Kristina <kr.hranovska@gmail.com>
2024-06-12 19:55:20 +0300

    update order

--- models/order.js
+++ models/order.js
@@ -3,7 +3,13 @@ import Joi from 'joi';
 import { commetRegax, valueNumber } from '../helper/constant.js';
 import mongooseError from '../helper/mongooseError.js';
 
-const orderShema = new Schema({
+const orderItemSchema = new Schema({
+    nameProduct: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    total: { type: String, required: true }
+}, { _id: false });
+
+const orderSchema = new Schema({
     firstName: {
         type: String,
         required: true,
@@ -17,6 +23,14 @@ const orderShema = new Schema({
         match: [valueNumber, 'Invalid phone number format. Use XX-XXX-XXXX'],
         required: true,
     },
+    selectRegion: {
+        type: String,
+        default: null,
+    },
+    selectCity: {
+        type: String,
+        default: null,
+    },
     comment: {
         type: String,
         match: [commetRegax, 'A comment can only contain Latin characters, numbers, and symbols .,!?/-"():;'],
@@ -25,17 +39,35 @@ const orderShema = new Schema({
     status: {
         type: String,
         default: 'Order accepted',
+    },
+    orderItems: {
+        type: [orderItemSchema],
+        required: true
+    },
+    totalPrice: {
+        type: Number,
+        required: true
     }
 }, { versionKey: false });
 
-orderShema.post('save', mongooseError);
+orderSchema.post('save', mongooseError);
+
+export const Order = model('order', orderSchema);
+
+const joiOrderItemSchema = Joi.object({
+    nameProduct: Joi.string().required(),
+    quantity: Joi.number().required(),
+    total: Joi.string().required()
+});
 
 export const createOrderSchema = Joi.object({
-    name: Joi.string().required(),
+    firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     phone: Joi.string().required(),
+    selectRegion: Joi.string().allow(null).empty(''),
+    selectCity: Joi.string().allow(null).empty(''),
     comment: Joi.string().max(300).pattern(commetRegax),
-    status: Joi.string()
-})
-
-export const Order = model('order', orderShema);
+    status: Joi.string(),
+    orderItems: Joi.array().items(joiOrderItemSchema).required(),
+    totalPrice: Joi.number().required()
+});
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -7,7 +7,63 @@ const orderRouter = express.Router();
 
 /**
  * @swagger
- * /order:
+ * components:
+ *   schemas:
+ *     OrderItem:
+ *       type: object
+ *       properties:
+ *         nameProduct:
+ *           type: string
+ *           description: Name of the product
+ *         quantity:
+ *           type: number
+ *           description: Quantity of the product
+ *         total:
+ *           type: string
+ *           description: Total price for the product
+ *     Order:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - phone
+ *         - orderItems
+ *         - totalPrice
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: First name of the customer
+ *         lastName:
+ *           type: string
+ *           description: Last name of the customer
+ *         phone:
+ *           type: string
+ *           description: Phone number of the customer
+ *         selectRegion:
+ *           type: string
+ *           description: Selected region
+ *         selectCity:
+ *           type: string
+ *           description: Selected city
+ *         comment:
+ *           type: string
+ *           description: Comment for the order
+ *         status:
+ *           type: string
+ *           description: Status of the order
+ *           default: Order accepted
+ *         orderItems:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/OrderItem'
+ *         totalPrice:
+ *           type: number
+ *           description: Total price for the order
+ */
+
+/**
+ * @swagger
+ * /orders:
  *   post:
  *     summary: Creates a new order
  *     tags: [Orders]
@@ -27,6 +83,7 @@ const orderRouter = express.Router();
  *       400:
  *         description: Bad request
  */
+
 
 orderRouter.post('/', validateBody(createOrderSchema), postOrders);
 
